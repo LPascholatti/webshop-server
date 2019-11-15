f9941ea8886c69eb1b920880f6ae59a81840d96b
LPascholatti <lucaspascholatti@gmail.com>
2019-11-15 14:41:43 +0100

    Add productId in comment model

--- comment/model.js
+++ comment/model.js
@@ -13,6 +13,11 @@ const Comment = sequelize.define(
     {
       type: Sequelize.STRING,
       allowNull: false
+    },
+    productId: 
+    {
+      type: Sequelize.INTEGER,
+      allowNull: false
     }
   }
 )
